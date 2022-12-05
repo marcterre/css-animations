654042e10b35a9169436a18073fad4090a9fc968
Marc Terre <marc.terre@outlook.de>
2022-12-05 21:52:43 +0100

    add image

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const firstDiv = document.querySelector('[data-js="firstDiv"]');
 const secondDiv = document.querySelector('[data-js="secondDiv"]');
 const button1 = document.querySelector('[data-js="button1"]');
 const button2 = document.querySelector('[data-js="button2"]');
+const image = document.querySelector('[data-js="image"]');
+
+button1.addEventListener("click", () => {
+  image.classList.toggle("img-hidden");
+});
 
 button1.addEventListener("mouseover", () => {
   button1.textContent = "🐝";
   button1.style.backgroundColor = "green";
+
   button1.addEventListener("mouseout", () => {
     button1.textContent = "Click me";
     button1.style.backgroundColor = "";
@@ -22,9 +28,11 @@ button2.addEventListener("click", () => {
     button2.style.color = "white";
     button2.classList.add("button_2-clicked");
   } else {
-    button2.textContent = startText;
-    button2.style.backgroundColor = "";
-    button2.style.color = "";
-    button2.classList.remove("button_2-clicked");
+    button2.addEventListener("mouseout", () => {
+      button2.textContent = startText;
+      button2.style.backgroundColor = "";
+      button2.style.color = "";
+      button2.classList.remove("button_2-clicked");
+    });
   }
 });
